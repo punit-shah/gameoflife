08aac80fd0763a0c70968cb9cf1ccf6451cabc63
Punit Shah <punit.shah@bbc.co.uk>
2017-02-13 23:31:33 +0000

    added test for scenario 1

--- spec/golSpec.js
+++ spec/golSpec.js
@@ -1,19 +1,42 @@
 describe('Game', function () {
     var evolve = require('../script/gol')().evolve;
 
-    it('can evolve an inital empty grid', function () {
+    it('can evolve an inital empty grid', () => {
         var initialState = [
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
         ];
 
         var resultState = [
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
         ];
 
         expect(evolve(initialState)).toEqual(resultState);
+    });
+
+    it('can evolve a live cell with fewer than two neighbours', () => {
+        var noNeighbours = [
+            [0, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0]
+        ];
+
+        var oneNeighbour = [
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0]
+        ];
+
+        var resultState = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ];
+
+        expect(evolve(noNeighbours)).toEqual(resultState);
+        expect(evolve(oneNeighbour)).toEqual(resultState);
     });
 });
