5839fcf565b51a6e0269ae4a1450c192ff1e2f26
Punit Shah <punit.shah@bbc.co.uk>
2017-02-14 00:55:17 +0000

    create new grid with same no. of rows/columns as previous grid
    all cells dead to pass scenario 1
    todo: count number of alive neighbours, and determine cell state (alive/dead) based on this

--- script/gol.js
+++ script/gol.js
@@ -1,12 +1,27 @@
 module.exports = function () {
+    const ALIVE = 1;
+    const DEAD = 0;
 
     function evolve(gridState) {
-        var newState = cloneArray(gridState);
-        return newState;
-    }
+        const rows = gridState.length;
+        const cols = gridState[0].length;
 
-    function cloneArray(array) {
-        return array.slice(0);
+        // newState - new grid with same number of rows and columns as gridState
+        var newState = new Array(rows);
+        for (let i = 0; i < rows; i++) {
+            newState[i] = new Array(cols);
+        }
+
+        // find state for each cell in grid
+        for (let x = 0; x < rows; x++) {
+            for (let y = 0; y < cols; y++) {
+                newState[x][y] = DEAD;
+
+                // todo: determine state depending on number of alive neighbours
+            }
+        }
+
+        return newState;
     }
 
     return {
